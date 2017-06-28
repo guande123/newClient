df323e5a04ec5525590763f3da8c1b006ddb695e
Gordon <987945799@qq.com>
2017-06-28 10:17:50 +0800

    初试化项目,并实现radioGroup3

---
+++ newClient/app/src/main/java/com/gdcp/zgd/newclient/GuideActivity.java
@@ -0,0 +1,27 @@
+package com.gdcp.zgd.newclient;
+
+/**
+ * Created by yls on 2017/6/28.
+ */
+
+public class GuideActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutRes() {
+        return 0;
+    }
+}
