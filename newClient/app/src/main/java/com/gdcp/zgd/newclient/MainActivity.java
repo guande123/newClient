5286e0651c7dfe7f0a8834b410d851ac0afe18a9
Gordon <987945799@qq.com>
2017-06-28 10:05:45 +0800

    初试化项目,并实现radioGroup

---
+++ newClient/app/src/main/java/com/gdcp/zgd/newclient/MainActivity.java
@@ -0,0 +1,25 @@
+package com.gdcp.zgd.newclient;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.activity_main;
+    }
+}
